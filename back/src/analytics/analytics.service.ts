5884d50aa38ecb380ab4d291cdcca28c445a8bb3
DARKLEGENT-AI <DARKLEGENT-AI@users.noreply.github.com>
2026-05-11 14:03:06 +0400

    fix pdf document import

--- back/src/analytics/analytics.service.ts
+++ back/src/analytics/analytics.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import * as PDFDocument from 'pdfkit'
+import PDFDocument = require('pdfkit')
 import * as ExcelJS from 'exceljs'
 import * as fs from 'fs'
 import * as path from 'path'
