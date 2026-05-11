b04ec93fec9bd0dea94be211f572834449cd5c1d
DARKLEGENT-AI <DARKLEGENT-AI@users.noreply.github.com>
2026-05-11 14:10:21 +0400

    update frontend api url

--- front/src/shared/constants/axios.ts
+++ front/src/shared/constants/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getAuthToken } from './auth'
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'https://diplom-back-fmvrybuzv-darklgnt.vercel.app/v1'
+  import.meta.env.VITE_API_URL || 'https://diplom-back-alpha.vercel.app/v1'
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
